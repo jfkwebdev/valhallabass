8a64da703ac5a15da7222bb57b0da0f416e2c662
jfkwebdev <jfkwebdev@gmail.com>
2022-09-16 18:38:56 -0400

    cleanup

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const linklist = document.querySelector('#link-list');
 const bandcamp = 'https://valhallabass.bandcamp.com/album/by-the-magnolia'
 const insta = 'http://instagram.com/valhallabass'
-const tiktok = 'http://tiktok.com/valhallabass'
+const tiktok = 'https://www.tiktok.com/@valhalla_bass'
 
 linklist.innerHTML =
     `<ul>
@@ -10,5 +10,7 @@ linklist.innerHTML =
         <li><a href="${tiktok}">TikTok</a></li>
     </ul>
     `;
+
+    //second ackground image , url(./layingitdown_files/287979606_3172554536322667_3956443757411855216_n.jpg)
 
     
