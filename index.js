c2bf881cfd73bcff751d7b73ced51ed7f7ca32be
jfkwebdev <jfkwebdev@gmail.com>
2022-10-28 12:44:09 -0400

    remove the homer quote XD

--- index.js
+++ index.js
@@ -31,23 +31,6 @@ function indexHeader() {
     column.innerHTML = `
     <iframe width="666" height="315" src="https://www.youtube.com/embed/jc8y61pfQ6s" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     <p>
-        Sing, Goddess, Achilles' rage,
-        Black and murderous, that cost the Greeks
-        Incalculable pain, pitched countless souls
-        Of heroes into Hades' dark,
-        And left their bodies to rot as feasts
-        For dogs and birds, as Zeus' will was done.
-            Begin with the clash between Agamemnon--
-        The Greek warlord--and godlike Achilles.
-        
-           Which of the immortals set these two
-        At each other's throats?
-                                            Apollo
-        Zeus' son and Leto's, offended
-        By the warlord. Agamemnon had dishonored
-        Chryses, Apollo's priest, so the god
-        Struck the Greek camp with plague,
-        And the soldiers were dying of it.
     </p>`
 }
 
